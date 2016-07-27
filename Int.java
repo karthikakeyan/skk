699d52f459f1aa630c547357d8712c042a002bea
karthikakeyan <karthikakeyan@users.noreply.github.com>
2016-07-27 12:25:14 +0530

    Update and rename Int.java to My.java

--- Int.java
+++
@@ -1,8 +0,0 @@
-public class Int extends  Applicant{
-	int employeeId;
-	public Int (int applicantId,String applicantName,int yrsOfExp, String skills, double expSalary, int employeeId){
-		super(applicantId,applicantName,yrsOfExp,skills,expSalary);
-		this.employeeId=employeeId;
-	}
-
-}
